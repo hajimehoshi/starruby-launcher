9fcda16987f897bf5185416773e6044f0b3bc2be
unknown <hajimehoshi@gmail.com>
2008-10-29 19:33:00 +0900

    Modified something

--- main.d
+++ main.d
@@ -4,5 +4,6 @@ import dfl.all;
 import srl.MainForm;
 
 void main() {
+  Application.autoCollect = false; // for Drag & Drop
   Application.run(new MainForm());
 }
--- srl/MainForm.d
+++ srl/MainForm.d
@@ -1,47 +1,87 @@
 module srl.MainForm;
 
 import dfl.all;
+import dfl.internal.utf;
 
 public class MainForm : Form {
 
+  private string fileName;
+
   private Panel titlePanel;
   private Panel mainPanel;
+  private Label notationLabel;
 
   public this() {
-    this.backColor = Color(0xff, 0xdd, 0xcc, 0xcc);
-    this.clientSize = Size(640, 480);
-    this.font = new Font("Georgia", 30f,
-                         FontStyle.REGULAR, GraphicsUnit.POINT, FontSmoothing.ON);
-    this.startPosition = FormStartPosition.CENTER_SCREEN;
-    this.text = "Star Ruby Launcher";
-    // Title Panel
-    this.titlePanel = new Panel();
-    this.titlePanel.backColor = Color(0xff, 0x99, 0x33, 0x33);
-    this.controls.add(this.titlePanel);
-    Label titleLabel = new Label();
-    titleLabel.autoSize = true;
-    titleLabel.foreColor = Color(0xff, 0xff, 0xff, 0xff);
-    titleLabel.location = Point(30, 15);
-    titleLabel.text = "Star Ruby Launcher";
-    this.titlePanel.controls.add(titleLabel);
-    // Main Panel
-    this.mainPanel = new Panel();
-    this.mainPanel.backColor = Color(0xff, 0xff, 0xff, 0xff);
-    this.controls.add(this.mainPanel);
+    with (this) {
+      allowDrop = true;
+      backColor = Color(0xff, 0xdd, 0xcc, 0xcc);
+      clientSize = Size(640, 480);
+      font = new Font("Georgia", 12f,
+                      FontStyle.REGULAR, GraphicsUnit.POINT, FontSmoothing.ON);
+      startPosition = FormStartPosition.CENTER_SCREEN;
+      text = "Star Ruby Launcher";
+    }
+    with (this.titlePanel = new Panel()) {
+      backColor = Color(0xff, 0x99, 0x33, 0x33);
+      parent = this;
+    }
+    Label titleLabel;
+    with (titleLabel = new Label()) {
+      autoSize = true;
+      font = new Font(this.font.name, 30f,
+                      this.font.style, this.font.unit, FontSmoothing.ON);
+      foreColor = Color(0xff, 0xff, 0xff, 0xff);
+      location = Point(30, 15);
+      parent = this.titlePanel;
+      text = "Star Ruby Launcher";
+    }
+    with (this.mainPanel = new Panel()) {
+      backColor = Color(0xff, 0xff, 0xff, 0xff);
+      parent = this;
+    }
+    with (this.notationLabel = new Label()) {
+      autoSize = true;
+      font = new Font(this.font.name, 20f,
+                      this.font.style, this.font.unit, FontSmoothing.ON);
+      foreColor = Color(0xff, 0x33, 0x22, 0x22);
+      location = Point(30, 15);
+      parent = this.mainPanel;
+      text = "Drag and Drop your Ruby script here!";
+    }
+  }
+
+  protected override void onDragOver(DragEventArgs e) {
+    super.onDragEnter(e);
+    if (e.data.getDataPresent(DataFormats.fileDrop)) {
+      e.effect = e.allowedEffect & DragDropEffects.COPY;
+    }
+  }
+
+  protected override void onDragDrop(DragEventArgs e) {
+    Data data = e.data.getData(DataFormats.fileDrop, false);
+    string[] fileNames = data.getStrings();
+    if (0 < fileNames.length) {
+      this.fileName = fileNames[0];
+      this.notationLabel.text = std.path.getBaseName(this.fileName);
+    }
   }
 
   protected override void onLayout(LayoutEventArgs e) {
     super.onLayout(e);
     Rect rect;
-    rect.x      = 0;
-    rect.y      = 0;
-    rect.width  = this.clientSize.width;
-    rect.height = 80;
+    with (rect) {
+      x      = 0;
+      y      = 0;
+      width  = this.clientSize.width;
+      height = 80;
+    }
     this.titlePanel.bounds = rect;
-    rect.x      = 0;
-    rect.y      = 110;
-    rect.width  = this.clientSize.width;
-    rect.height = this.clientSize.height - 110 - 20;
+    with (rect) {
+      x      = 0;
+      y      = 110;
+      width  = this.clientSize.width;
+      height = this.clientSize.height - 110 - 20;
+    }
     this.mainPanel.bounds = rect;
   }
 }
