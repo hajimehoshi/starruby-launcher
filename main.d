4b97394c5af37a41edf443c69de4733f43e21acf
unknown <hajimehoshi@gmail.com>
2008-10-30 02:49:04 +0900

    Added win32

--- main.d
+++ main.d
@@ -3,7 +3,14 @@ module main;
 import dfl.all;
 import srl.MainForm;
 
-void main() {
+void main(string[] args) {
   Application.autoCollect = false; // for Drag & Drop
-  Application.run(new MainForm());
+  MainForm mainForm = new MainForm();
+  if (1 < args.length) {
+    string arg = args[1];
+    if (mainForm.isAcceptableFileName(arg)) {
+      mainForm.fileName = arg;
+    }
+  }
+  Application.run(mainForm);
 }
--- srl/MainForm.d
+++ srl/MainForm.d
@@ -1,23 +1,23 @@
 module srl.MainForm;
 
 import dfl.all;
-import dfl.internal.utf;
+import win32.psapi;
 
 public class MainForm : Form {
-
-  private string fileName;
 
   private Panel titlePanel;
   private Panel mainPanel;
   private Label notationLabel;
 
   public this() {
+    Font createFont(float size) {
+      return new Font("Georgia", size,
+                      FontStyle.REGULAR, GraphicsUnit.POINT, FontSmoothing.ON);
+    }
     with (this) {
       allowDrop = true;
       backColor = Color(0xff, 0xdd, 0xcc, 0xcc);
       clientSize = Size(640, 480);
-      font = new Font("Georgia", 12f,
-                      FontStyle.REGULAR, GraphicsUnit.POINT, FontSmoothing.ON);
       startPosition = FormStartPosition.CENTER_SCREEN;
       text = "Star Ruby Launcher";
     }
@@ -28,8 +28,7 @@ public class MainForm : Form {
     Label titleLabel;
     with (titleLabel = new Label()) {
       autoSize = true;
-      font = new Font(this.font.name, 30f,
-                      this.font.style, this.font.unit, FontSmoothing.ON);
+      font = createFont(30f);
       foreColor = Color(0xff, 0xff, 0xff, 0xff);
       location = Point(30, 15);
       parent = this.titlePanel;
@@ -41,29 +40,58 @@ public class MainForm : Form {
     }
     with (this.notationLabel = new Label()) {
       autoSize = true;
-      font = new Font(this.font.name, 20f,
-                      this.font.style, this.font.unit, FontSmoothing.ON);
+      font = createFont(20f);
       foreColor = Color(0xff, 0x33, 0x22, 0x22);
       location = Point(30, 15);
       parent = this.mainPanel;
-      text = "Drag and Drop your Ruby script here!";
+    }
+    this.updateNotationLabel();
+  }
+
+  public string fileName() {
+    return this._fileName;
+  }
+  public string fileName(string _fileName) in {
+    assert(this.isAcceptableFileName(_fileName));
+  } body {
+    this._fileName = _fileName;
+    this.updateNotationLabel();
+    return _fileName;
+  }
+  private string _fileName;
+
+  public bool isAcceptableFileName(string fileName) {
+    return (fileName is null) ||
+      (std.file.exists(fileName) && std.file.isfile(fileName));
+  }
+
+  private void updateNotationLabel() {
+    assert(this.notationLabel);
+    if (this.fileName) {
+      this.notationLabel.text = std.path.getBaseName(this.fileName);
+    } else {
+      this.notationLabel.text = "Drag and Drop your Ruby script here!";
     }
   }
 
   protected override void onDragOver(DragEventArgs e) {
     super.onDragEnter(e);
     if (e.data.getDataPresent(DataFormats.fileDrop)) {
-      e.effect = e.allowedEffect & DragDropEffects.COPY;
+      Data data = e.data.getData(DataFormats.fileDrop, false);
+      string[] fileNames = data.getStrings();
+      if (0 < fileNames.length) {
+        string fileName = fileNames[0];
+        if (this.isAcceptableFileName(fileName)) {
+          e.effect = e.allowedEffect & DragDropEffects.MOVE;
+        }
+      }
     }
   }
 
   protected override void onDragDrop(DragEventArgs e) {
+    super.onDragDrop(e);
     Data data = e.data.getData(DataFormats.fileDrop, false);
-    string[] fileNames = data.getStrings();
-    if (0 < fileNames.length) {
-      this.fileName = fileNames[0];
-      this.notationLabel.text = std.path.getBaseName(this.fileName);
-    }
+    this.fileName = data.getStrings()[0];
   }
 
   protected override void onLayout(LayoutEventArgs e) {
@@ -83,5 +111,9 @@ public class MainForm : Form {
       height = this.clientSize.height - 110 - 20;
     }
     this.mainPanel.bounds = rect;
+  }
+
+  private void run() {
+    //CreateProcess();
   }
 }
